9a3f4e631de0ea5e057bc9064bd7a8eb4314ad74
MRITUNJAY KUMAR SOURAV <mkignito@gmail.com>
2019-09-18 20:19:44 +0530

    Add files via upload

---
+++ test-c4.py
@@ -0,0 +1,13 @@
+print(ord('H'))
+print(ord('e'))
+print(ord('\n'))
+x = b'abc'
+print(type(x))
+x= 'こんにちは'
+print(type(x))
+x= u'こんにちは'
+print(type(x))
+
+inp=input('Europe floor?')
+usf=int(inp)+1
+print('US floor', usf)
